e497386c1337d9dad2c7aab72a6c43af0b32bb90
yashpriyam <yashpriyam74@gmail.com>
2022-05-31 12:11:11 +0530

    create trade executer class, call api in this class's method and log data with a random message

---
+++ src/services/TradeExecuter.ts
@@ -0,0 +1,76 @@
+import axios from "axios";
+import { LogzioLogger } from "../lib/logzioLogger";
+
+class TradeExecuter {
+  async executeTrade(tradeData: any) {
+    const url =
+      process.env.NODE_ENV === "dev"
+        ? process.env.DEV_SERVER_URL
+        : process.env.PROD_SERVER_URL;
+
+    try {
+      const {
+        tradePair,
+        askPriceExchange,
+        bidPriceExchange,
+        percentage_diffr,
+        quantity,
+        askPrice,
+        bidPrice,
+      } = tradeData;
+
+      const purchaseTradeData = {
+        askPriceExchange,
+        percentage_diffr,
+        purchageQuantity: quantity,
+        tradePair,
+        askPrice,
+        tradeValue: askPrice * quantity,
+      };
+
+      const sellTradeData = {
+        bidPriceExchange,
+        percentage_diffr,
+        sellQuantity: quantity,
+        tradePair,
+        bidPrice,
+        tradeValue: bidPrice * quantity,
+      };
+
+      const purchaseResponse = await axios.post(
+        `${url}/purchase`,
+        purchaseTradeData
+      );
+
+      const sellResponse = await axios.post(`${url}/sell`, sellTradeData);
+
+      LogzioLogger.info(
+        JSON.stringify({
+          purchaseTradeData,
+          sellTradeData,
+          purchaseResponseData: purchaseResponse.data,
+          sellResponseData: sellResponse.data,
+        })
+      );
+
+      this.randomMessageExecuter();
+    } catch (error) {
+      LogzioLogger.error("Error occured during purchase or selling");
+    }
+  }
+
+  randomMessageExecuter() {
+    const messages = [
+      "Executed arbitrage opportunity successfully",
+      "Missed arbitrage opportunity",
+    ];
+
+    let randomIndex = Math.round(Math.random() * messages.length);
+
+    if (randomIndex === messages.length) randomIndex = randomIndex - 1;
+
+    LogzioLogger.info(JSON.stringify({ message: messages[randomIndex] }));
+  }
+}
+
+export const tradeExecuterInstance = new TradeExecuter();

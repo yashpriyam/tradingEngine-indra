011b18baa67e96ad287878f3fcefe9f27741554b
Yash priyam <yashpriyam74@gmail.com>
2022-06-07 12:34:50 +0530

    add base interface implementation

--- src/@types/index.d.ts
+++ src/@types/index.d.ts
@@ -5,3 +5,31 @@ type orderBookMap = {
     };
   };
 };
+
+
+interface Action {
+  execute(arg: any): void
+}
+interface PriceOracle {
+  _handlers: Map<any, any>;
+  isConnected: boolean;
+  _ws: any;
+  setHandler(method: string, callback: (params: any) => void): void
+  heartBeat(): void;
+  isMultiStream(message: { stream: any }): boolean
+  getMessageStream(wsInstance: any, dataFormat: any): void
+  _createSocket(wsUrl: string): any
+  getPathValue(payload: { [x: string]: any }, path: string): { [x: string]: any }
+  subscribeStream(subscriberObject: Object, wsInstance: any): void
+
+}
+interface TradeExecutor {
+  placeOrder(): Boolean
+  cancelOrder(): true
+  checkOrderStatus(): true
+  getWalletBalance(exchange: string): true
+}
+interface Strategy {
+  start(): void
+  check(): void
+}
